a8ebf73804d4e5e9845ba7305b9077e0d0c94e7f
C-Florian <florianchereau@hotmail.fr>
2025-05-28 00:15:59 +0200

    add a class BaseGeometry (based on 5-base_geometry.py)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+class BaseGeometry:
+    """empty class BaseGeometry"""
+    def area(self):
+        """function that write a class BaseGeometry (based on 5-base_geometry.py)"""
+        raise Exception("area() is not implemented")

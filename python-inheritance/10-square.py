c51011ce211265142e2db975380d62e0fc93ccdd
C-Florian <florianchereau@hotmail.fr>
2025-05-30 19:13:10 +0200

    correction of the task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""square #1"""
+"""Module that defines a Square class."""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
-    """class Square that inherits from Rectangle"""
+    """Square class that inherits from Rectangle."""
 
     def __init__(self, size):
-        """initialize square"""
+        """Initialize square with size.
+
+        Args:
+            size (int): size of the square (must be > 0)
+        """
         self.integer_validator("size", size)
         self.__size = size
-    
-     def area(self):
-         """define the area of square"""
-        return self.__size * self.__size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return the square description."""
+        return f"[Square] {self.__size}/{self.__size}"
+

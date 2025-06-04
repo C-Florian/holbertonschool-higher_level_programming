bd684dca1bfb5ee73d6060cf40569708527e0861
C-Florian <florianchereau@hotmail.fr>
2025-06-04 15:27:42 +0200

    add a class that defines a student by attributes

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,13 +2,28 @@
 """student to JSON"""
 
 class Student:
-    """class that defines a student"""
-    def __init__(self, first_name, last_name, age):
+    """class that defines a student by attributes"""
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+
+    def __init__(self, first_name, last_name, age):
+        """
+        initializes a new Student instance
+
+        args:
+            first_name (str): student first name
+            last_name (str): student last name
+            age (int): student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        """ return a  dictionnary that store all the attribute"""
+        """
+        returns a dictionary that store the student attributes
+
+        return:
+            dict: attibutes of the student object
+        """
         return self.__dict__
+

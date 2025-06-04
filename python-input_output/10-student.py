7ad3d0639a9075673a985cac4aa703e0aedb787e
C-Florian <florianchereau@hotmail.fr>
2025-06-04 16:32:53 +0200

    add a class that defines a student by attributes with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,28 +4,33 @@
 class Student:
     """class that defines a student by attributes"""
 
+
     def __init__(self, first_name, last_name, age):
-        """initialize a new student
+        """
+        initializes a new Student instance
 
         args:
-            first_name
-            last_name
-            age"""
-    
+            first_name (str): student first name
+            last_name (str): student last name
+            age (int): student age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-                """return a  dictionnary that store all the attribute
-                if attrs was a string, return exclusively this attributes
+    def to_json(self):
+        """
+        returns a dictionary that store the student attributes
 
-                args:
-                    attrs
+        if `attrs` is a list of strings, only the attributes listed
+        will be returned (if they exist)
 
-                returns:
-                    dict"""
-
+        args:
+            attrs (list, optional): list of attribute names to include
+        
+        return:
+            dict: filtered or full dictionary of student attributes
+        """
         if isinstance(attrs, list) and all(type(attr) == str for attr in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
